fec702f73ea868d1910682ec6bcd1818af486fb3
Daniel Victor <danielvictorcarneiro21@gmail.com>
2024-10-07 12:55:04 -0300

    done

---
+++ recomendar.pl
@@ -0,0 +1,107 @@
+% Base de conhecimento dos artistas
+artista(billie_eilish, generos([pop, alternative, electropop]), sensacao([melancolica, profunda, relaxante]), ritmo([lento, moderado])).
+artista(kendrick_lamar, generos([hiphop, rap, jazz_rap]), sensacao([profunda, reflexiva, energizante]), ritmo([rapido, moderado])).
+artista(ludovico_einaudi, generos([classica, minimalista]), sensacao([calmante, inspiradora]), ritmo([lento])).
+artista(taylor_swift, generos([pop, country, folk]), sensacao([romantica, alegre, reflexiva]), ritmo([moderado, rapido])).
+artista(avicii, generos([edm, house, dance]), sensacao([energizante, alegre]), ritmo([rapido])).
+artista(radiohead, generos([rock_alternativo, experimental]), sensacao([melancolica, profunda, introspectiva]), ritmo([moderado, lento])).
+artista(john_coltrane, generos([jazz, bebop, free_jazz]), sensacao([reflexiva, intensa, emocional]), ritmo([moderado, rapido])).
+artista(bob_marley, generos([reggae]), sensacao([relaxante, positiva]), ritmo([moderado])).
+artista(ariana_grande, generos([pop, rnb]), sensacao([alegre, animada]), ritmo([rapido, moderado])).
+artista(joao_gilberto, generos([bossa_nova, mpb]), sensacao([relaxante, romantica]), ritmo([lento, moderado])).
+artista(led_zeppelin, generos([rock, hard_rock, blues_rock]), sensacao([intensa, energetica, poderosa]), ritmo([moderado, rapido])).
+artista(caetano_veloso, generos([mpb, tropicalia, samba]), sensacao([reflexiva, alegre, romantica]), ritmo([lento, moderado])).
+artista(bruno_mars, generos([pop, rnb, funk]), sensacao([alegre, dancante, animada]), ritmo([rapido, moderado])).
+artista(zeze_di_camargo_e_luciano, generos([sertanejo, sertanejo_romantico]), sensacao([romantica, emocional]), ritmo([moderado, lento])).
+artista(arctic_monkeys, generos([rock_alternativo, indie_rock]), sensacao([intensa, energetica, introspectiva]), ritmo([moderado, rapido])).
+artista(grupo_revelacao, generos([pagode, samba]), sensacao([alegre, dancante]), ritmo([moderado, rapido])).
+artista(j_balvin, generos([reggaeton, latin_pop, trap_latino]), sensacao([dancante, animada]), ritmo([rapido])).
+artista(cartola, generos([samba, mpb]), sensacao([romantica, nostalgica]), ritmo([lento, moderado])).
+artista(eminem, generos([hiphop, rap]), sensacao([agressiva, energetica, reflexiva]), ritmo([rapido, moderado])).
+artista(jorge_e_mateus, generos([sertanejo, sertanejo_universitario]), sensacao([romantica, alegre]), ritmo([moderado, rapido])).
+artista(tame_impala, generos([indie, psychedelic_rock, pop_alternativo]), sensacao([introspectiva, relaxante, criativa]), ritmo([lento, moderado])).
+artista(gal_costa, generos([mpb, samba]), sensacao([romantica, alegre, nostalgica]), ritmo([moderado, lento])).
+artista(shakira, generos([pop, latin_pop]), sensacao([dancante, energetica, romantica]), ritmo([rapido, moderado])).
+artista(tom_jobim, generos([bossa_nova, mpb]), sensacao([relaxante, romantica, introspectiva]), ritmo([lento, moderado])).
+artista(anitta, generos([funk_brasileiro, pop, reggaeton]), sensacao([dancante, energetica]), ritmo([rapido])).
+artista(rihanna, generos([pop, rnb, reggae]), sensacao([energetica, sensual]), ritmo([moderado, rapido])).
+artista(jorge_ben_jor, generos([samba_rock, mpb, bossa_nova]), sensacao([alegre, dancante, leve]), ritmo([moderado])).
+artista(miles_davis, generos([jazz, bebop, fusion]), sensacao([intensa, reflexiva, improvisada]), ritmo([lento, moderado])).
+artista(nina_simone, generos([jazz, blues, soul]), sensacao([profunda, emocional, reflexiva]), ritmo([lento, moderado])).
+artista(frank_sinatra, generos([jazz, swing, traditional_pop]), sensacao([romantica, relaxante, nostalgica]), ritmo([moderado, lento])).
+artista(david_bowie, generos([rock, glam_rock, pop]), sensacao([intensa, criativa, enigmatica]), ritmo([moderado, rapido])).
+artista(stevie_wonder, generos([soul, funk, rnb]), sensacao([alegre, inspiradora, emocional]), ritmo([moderado, rapido])).
+artista(bjork, generos([experimental, electronic, art_pop]), sensacao([criativa, introspectiva, energetica]), ritmo([moderado, lento])).
+artista(bon_iver, generos([indie_folk, folk, electronic]), sensacao([relaxante, introspectiva, melancolica]), ritmo([lento, moderado])).
+artista(lady_gaga, generos([pop, dance, electronic]), sensacao([energetica, provocativa, dancante]), ritmo([rapido, moderado])).
+artista(bob_dylan, generos([folk, rock, country]), sensacao([profunda, reflexiva, critica]), ritmo([lento, moderado])).
+artista(the_beatles, generos([rock, pop, psychedelic_rock]), sensacao([alegre, introspectiva, enigmatica]), ritmo([moderado, rapido])).
+artista(the_weeknd, generos([rnb, pop, electronic]), sensacao([energetica, sombria, sensual]), ritmo([moderado, rapido])).
+artista(bad_bunny, generos([trap_latino, reggaeton, latin_pop]), sensacao([energetica, dancante, provocativa]), ritmo([rapido, moderado])).
+artista(dua_lipa, generos([pop, electronic, disco]), sensacao([dancante, energetica, alegre]), ritmo([rapido, moderado])).
+artista(los_fabulosos_cadillacs, generos([ska, rock, latin_rock]), sensacao([alegre, dancante, energetica]), ritmo([moderado, rapido])).
+artista(rosalia, generos([flamenco, pop, reggaeton]), sensacao([intensa, enigmatica, provocativa]), ritmo([rapido, moderado])).
+artista(the_strokes, generos([indie_rock, garage_rock, post_punk]), sensacao([intensa, energetica, introspectiva]), ritmo([moderado, rapido])).
+artista(norah_jones, generos([jazz, folk, pop]), sensacao([relaxante, introspectiva, suave]), ritmo([lento, moderado])).
+artista(seu_jorge, generos([mpb, samba_rock, funk]), sensacao([alegre, dancante, relaxante]), ritmo([moderado, rapido])).
+artista(alicia_keys, generos([rnb, soul, pop]), sensacao([romantica, emocional, inspiradora]), ritmo([moderado, lento])).
+artista(chico_buarque, generos([mpb, samba, bossa_nova]), sensacao([profunda, critica, romantica]), ritmo([lento, moderado])).
+artista(pink_floyd, generos([progressive_rock, psychedelic_rock]), sensacao([introspectiva, criativa, profunda]), ritmo([lento, moderado])).
+artista(lana_del_rey, generos([indie_pop, dream_pop]), sensacao([melancolica, introspectiva, romantica]), ritmo([lento, moderado])).
+artista(charlie_puth, generos([pop, rnb]), sensacao([alegre, dancante, emocional]), ritmo([rapido, moderado])).
+artista(ed_sheeran, generos([pop, folk_pop]), sensacao([romantica, alegre, reflexiva]), ritmo([moderado, lento])).
+artista(post_malone, generos([hiphop, trap, pop_rock]), sensacao([reflexiva, energizante, emocional]), ritmo([moderado, rapido])).
+artista(sza, generos([rnb, soul]), sensacao([profunda, emocional, sensual]), ritmo([moderado, lento])).
+artista(drake, generos([hiphop, rap, rnb]), sensacao([reflexiva, alegre, introspectiva]), ritmo([rapido, moderado])).
+artista(the_rolling_stones, generos([rock, blues_rock]), sensacao([energetica, intensa, reflexiva]), ritmo([moderado, rapido])).
+artista(fleetwood_mac, generos([rock, folk_rock]), sensacao([romantica, reflexiva, introspectiva]), ritmo([lento, moderado])).
+artista(kanye_west, generos([hiphop, rap, electronic]), sensacao([energizante, reflexiva, provocativa]), ritmo([rapido, moderado])).
+artista(adele, generos([pop, soul]), sensacao([emocional, reflexiva, romantica]), ritmo([lento, moderado])).
+artista(coldplay, generos([pop_rock, alternative_rock]), sensacao([alegre, introspectiva, emocional]), ritmo([moderado, rapido])).
+artista(maroon_5, generos([pop, funk_rock]), sensacao([alegre, dancante, romantica]), ritmo([rapido, moderado])).
+artista(imagine_dragons, generos([pop_rock, alternative_rock]), sensacao([energizante, inspiradora, intensa]), ritmo([moderado, rapido])).
+artista(beyonce, generos([pop, rnb, soul]), sensacao([energetica, poderosa, sensual]), ritmo([rapido, moderado])).
+artista(sam_smith, generos([pop, soul]), sensacao([emocional, introspectiva, reflexiva]), ritmo([lento, moderado])).
+artista(gilberto_gil, generos([mpb, samba, tropicalia]), sensacao([alegre, reflexiva, inspiradora]), ritmo([moderado, lento])).
+artista(marisa_monte, generos([mpb, samba]), sensacao([romantica, introspectiva, suave]), ritmo([lento, moderado])).
+artista(gabriel_o_pensador, generos([rap, hiphop]), sensacao([reflexiva, critica, energizante]), ritmo([rapido, moderado])).
+artista(raimundos, generos([rock, punk_rock]), sensacao([intensa, divertida, provocativa]), ritmo([rapido, moderado])).
+artista(pitty, generos([rock, rock_alternativo]), sensacao([intensa, rebelde, energética]), ritmo([rapido, moderado])).
+artista(zeca_pagodinho, generos([samba, pagode]), sensacao([alegre, relaxante, dançante]), ritmo([moderado, lento])).
+artista(maria_bethania, generos([mpb, samba]), sensacao([introspectiva, emocional, profunda]), ritmo([lento, moderado])).
+artista(nando_reis, generos([rock, mpb]), sensacao([romantica, alegre, reflexiva]), ritmo([moderado, lento])).
+artista(titas, generos([rock, punk_rock]), sensacao([intensa, energética, reflexiva]), ritmo([rapido, moderado])).
+artista(ivan_lins, generos([mpb, jazz]), sensacao([romantica, reflexiva, suave]), ritmo([lento, moderado])).
+artista(baiana_system, generos([axé, reggae, dub]), sensacao([energizante, dançante, alegre]), ritmo([rapido, moderado])).
+artista(los_hermanos, generos([rock_alternativo, mpb]), sensacao([melancolica, reflexiva, introspectiva]), ritmo([lento, moderado])).
+artista(jorge_aranha, generos([samba_rock, mpb]), sensacao([alegre, dançante, leve]), ritmo([moderado, rapido])).
+artista(cezar_menotti_e_fabiano, generos([sertanejo, sertanejo_universitario]), sensacao([romantica, alegre]), ritmo([moderado, rapido])).
+artista(tribalistas, generos([mpb, pop]), sensacao([alegre, introspectiva, suave]), ritmo([moderado, lento])).
+artista(elis_regina, generos([mpb, samba, bossa_nova]), sensacao([emocional, intensa, reflexiva]), ritmo([moderado, lento])).
+artista(falamansa, generos([forró]), sensacao([alegre, dançante, relaxante]), ritmo([rapido, moderado])).
+artista(luan_santana, generos([sertanejo, sertanejo_universitario]), sensacao([romantica, alegre]), ritmo([moderado, rapido])).
+artista(baroes_da_pisadinha, generos([arrocha, forró]), sensacao([dançante, alegre, divertida]), ritmo([rapido, moderado])).
+artista(ana_carolina, generos([mpb, pop]), sensacao([emocional, reflexiva, romântica]), ritmo([moderado, lento])).
+
+
+% Regras de recomendação
+recomendar_artista(Genero, Sensacao, Ritmo, Artista) :-
+    artista(Artista, generos(Generos), sensacao(Sensacoes), ritmo(Ritmos)),
+    member(Genero, Generos),
+    member(Sensacao, Sensacoes),
+    member(Ritmo, Ritmos).
+
+% Função para listar artistas recomendáveis
+listar_artistas_recomendaveis(Genero, Sensacao, Ritmo, Lista) :-
+    findall(Artista, recomendar_artista(Genero, Sensacao, Ritmo, Artista), Lista).
+
+% Predicado para fazer a consulta
+recomendar :-
+    write('Digite o genero (ex: pop, rock, jazz, mpb, edm, rnb, indie_rock, samba, pagode, etc): '), read(Genero),
+    write('Digite a sensacao (ex: alegre, reflexiva, intensa, relaxante, energizante, poderosa, instrospectiva, emocional, etc): '), read(Sensacao),
+    write('Digite o ritmo (ex: rapido, moderado, lento): '), read(Ritmo),
+    listar_artistas_recomendaveis(Genero, Sensacao, Ritmo, Lista),
+    write('Artistas recomendados: '), write(Lista), nl.
+
+% Consultar com "recomendar." no terminal para iniciar a recomendação
+
